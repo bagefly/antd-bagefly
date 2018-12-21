5db9564f382800dc92b8acf90642f07fcb2093a1
bagefly <huangxhe@126.com>
2018-12-21 17:21:35 +0800

    md

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import Button from 'antd/lib/button';
+import Icon from 'antd/lib/icon';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Button type="primary">Button</Button>
+        <Button type="primary"><Icon type="play-circle" />play</Button>
       </div>
     );
   }
